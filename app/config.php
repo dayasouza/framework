2a4c212110561fb14e23a6ec24aabb0f27b675db
Daya Souza <dayanalvesouza@gmail.com>
2024-01-03 17:39:15 -0300

    Criado cabecalho e rodapé da pagina

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php 
 define('APP', dirname(__FILE__));
 
-define('URL', 'http://localhost/framework/');
+define('URL', 'http://localhost/framework');
 
 define('APP_NOME', 'Curso de PHP7 Orientado a Objetos e MVC');
 
--- public/index.php
+++ public/index.php
@@ -8,12 +8,19 @@ include './../app/Libraries/Controller.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= APP_NOME?></title>
-</head>
+    <title><?= APP_NOME ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= URL ?>/public/css/style.css">
 <body>
     <?php
+        include '../app/Views/header.php';
         $rotas = new Rota();
+        include '../app/Views/footer.php';
     ?>
-    
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="<?= URL ?>/public/js/jquery.funcoes.js"></script>
 </body>
 </html>
