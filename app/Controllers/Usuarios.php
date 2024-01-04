dfa9b6e05810856e4f2f725e0185cf07d72721a7
Daya Souza <dayanalvesouza@gmail.com>
2024-01-04 14:29:31 -0300

    Adicioando comentários ao código!

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -7,16 +7,21 @@ class Usuarios extends Controller
     // Método para exibir a view de cadastro de usuários
     public function cadastrar()
     {
+        // Obtém os dados do formulário usando filter_input_array e aplica a filtragem
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+         // Verifica se o formulário foi submetido
         if (isset($formulario)) {
+            // Cria um array associativo com os dados do formulário, removendo espaços em branco desnecessários
             $dados = [
                 'nome' => trim($formulario['nome']),
                 'email' => trim($formulario['email']),
                 'senha' => trim($formulario['senha']),
                 'confirmar_senha' => trim($formulario['confirmar_senha'])
             ];
+            // Exibe os dados do formulário para fins de depuração
             var_dump($formulario);
         } else {
+            // Se o formulário não foi submetido, inicializa o array de dados com valores vazios
             $dados = [
                 'nome' => '',
                 'email' => '',
