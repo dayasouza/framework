034fa620516654dca296e5d8f44faada09a5b9e9
Daya Souza <dayanalvesouza@gmail.com>
2024-01-08 07:52:54 -0300

    Criado primeiro Helper para validar informações como nome e e-mail

---
+++ app/Helpers/Valida.php
@@ -0,0 +1,21 @@
+<?php
+
+class Valida {
+
+    public static function validarNome($nome){
+        if(!preg_match('/^([áÁàÀãÃâÂéÉèÈêÊíÍìÌóÓòÒõÕôÔúÚùÙçÇaA-zZ]+)+((\s[áÁàÀãÃâÂéÉèÈêÊíÍìÌóÓòÒõÕôÔúÚùÙçÇaA-zZ]+)+)?$/', $nome)):
+            return true;
+        else:
+            return false;
+        endif;
+    }
+
+    public static function validarEmail($email){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)):
+            return true;
+        else:
+            return false;
+        endif;
+    }
+
+}
