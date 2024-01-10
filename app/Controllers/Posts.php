3ba585aa80c6d74c1f04735233cf19dc82682734
Daya Souza <dayanalvesouza@gmail.com>
2024-01-10 12:01:06 -0300

    Criado a função cadastrar no contolador posts, a pagina de cadastrar post e inserido uma barra de navegação superior na view post/index

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
 class Posts extends Controller
 {
+    private $usuarioModel;
+
     public function __construct()
     {
         if (!Sessao::usuarioLogado()) {
@@ -9,7 +11,50 @@ class Posts extends Controller
     }
     public function index()
     {
-        $this-> view('posts/index');
+        $this->view('posts/index');
+    }
+
+    public function cadastrar()
+    {
+
+        $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        if (isset($formulario)) :
+            $dados = [
+                'titulo' => trim($formulario['titulo']),
+                'texto' => trim($formulario['texto']),
+                'titulo_erro' => '',
+                'texto_erro' => ''
+            ];
+
+            if (in_array("", $formulario)) :
+
+                if (empty($formulario['titulo'])) :
+                    $dados['titulo_erro'] = 'Preencha o campo titulo';
+                endif;
+
+                if (empty($formulario['texto'])) :
+                    $dados['texto_erro'] = 'Preencha o campo texto';
+                endif;
+            else :
+
+                echo "Pode cadastrar o post <hr>";
+
+                var_dump($formulario);
+
+            endif;
+
+
+        else :
+            $dados = [
+                'titulo' => '',
+                'texto' => '',
+                'titulo_erro' => '',
+                'texto_erro' => ''
+            ];
+
+        endif;
+
+
+        $this->view('posts/cadastrar', $dados);
     }
 }
-?>
