f3481dd29c9e6ee2ad92479b78540c23c8f823cb
Daya Souza <dayanalvesouza@gmail.com>
2024-01-12 18:49:28 -0300

    Criado a função para que o usuario que criou o post possa editar

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -45,4 +45,19 @@ class Post
 
         return $this-> db -> resultado();
     }
+
+    public function atualizar($dados)
+    {
+        $this->db->query("UPDATE posts SET titulo = :titulo, texto = :texto WHERE id = :id");
+
+        $this->db->bind("id", $dados['id']);
+        $this->db->bind("titulo", $dados['titulo']);
+        $this->db->bind("texto", $dados['texto']);
+
+        if ($this->db->executa()) :
+            return true;
+        else :
+            return false;
+        endif;
+    }
 }
