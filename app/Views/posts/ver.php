303fd2066d2fbf103aad34be98933c0791194f41
Daya Souza <dayanalvesouza@gmail.com>
2024-01-12 18:05:40 -0300

    Incluido o botão editar para fazer edição do post apenas pelo mesmo usuario que criou o post

--- app/Views/posts/ver.php
+++ app/Views/posts/ver.php
@@ -2,7 +2,7 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?= URL ?>/posts">Posts</a></li>
-            <li class="breadcrumb-item active" aria-current="page"><?= $dados['post'] -> titulo ?></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= $dados['post']->titulo ?></li>
         </ol>
     </nav>
     <div class="card text-center">
@@ -13,8 +13,15 @@
             <p class="card-text"><?= $dados['post']->texto ?></p>
         </div>
         <div class="card-footer text-muted">
-            Escrito por: <?= $dados['usuario'] -> nome ?> em <?= Valida::dataBr($dados['post'] -> criado_em) ?>
+            <small>
+                Escrito por: <?= $dados['usuario']->nome ?> em <?= Valida::dataBr($dados['post']->criado_em) ?>
+            </small>
         </div>
+        <?php 
+            if ($dados['post'] -> usuario_id == $_SESSION['usuario_id']) { ?>
+                <a href="<?= URL . '/posts/editar/' . $dados['post']->id ?>" class="btn btn-sm btn-primary">Editar</a>
+           <?php }
+        ?>
     </div>
 
 </div>
